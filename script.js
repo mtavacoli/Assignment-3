fc2d0fc06eedb878d4c11c801b30b359bbbaa113
Matin Tavacoli <mtavacoli33@gmail.com>
2022-12-20 12:51:06 -0600

    saving updates pseudo

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Assignment code here
 
+// I WANT to randomly generate a password that meets certain criteria
+// User clicks generate password button
+
+// Atleast 8 characters, but no more than 128 characters
+// ALERT PROMPT CONFIRM
+//Use a prompt to get info on what the user wants the password to be like
+// INPUT types
+// Whether the user wants lowercase, uppercase, numerical, and/or special characters
+//Use a confirm to get info on what characters the user wants
+
+// Site will generate a random password for the user
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log("generatePassword");
+  return "oft he king";
+}
 
 // Write password to the #password input
 function writePassword() {
